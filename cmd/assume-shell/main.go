89ec5cfe950941c27d017ae3563dbcce9082fadc
Erik Jansson <erik@lifesum.com>
2022-02-12 20:59:19 +0100

    Add release support

--- cmd/assume-shell/main.go
+++ cmd/assume-shell/main.go
@@ -10,8 +10,28 @@ import (
 	"github.com/comebackoneyear/assume-shell/pkg/shell"
 )
 
+var (
+	// Populated by goreleaser during build
+	version = "dev"
+	commit  = ""
+	date    = ""
+	arch    = ""
+)
+
 func main() {
+	flagVersion := flag.Bool("v", false, "prints current assume-shell version")
+
 	flag.Parse()
+
+	if *flagVersion {
+		if commit != "" {
+			fmt.Printf("assume-shell %s arch:%s commit:%s date:%s\n", version, arch, commit, date)
+		} else {
+			fmt.Printf("assume-shell %s build\n", version)
+		}
+		os.Exit(0)
+	}
+
 	argv := flag.Args()
 
 	assumeProfile := "default"
@@ -43,6 +63,6 @@ func init() {
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s [profile]\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "Usage: %s [-v|<profile name>]\n", os.Args[0])
 	flag.PrintDefaults()
 }
